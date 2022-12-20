328711cc7958b4859400359bec3b080467f611d8
Richard Munyao <richard.munyao@gmail.com>
2022-12-20 19:09:34 +0100

    limit date, month and year inputs to valid entries

--- assets/js/akan.js
+++ assets/js/akan.js
@@ -66,7 +66,7 @@ function displayNewName(newName) {
     console.log("in displayNewName");
     document.getElementById("name-major").innerHTML = `<h1> Your Akan Name is: ${newName}</h1> `;
     // To DO:
-    // 1. Limit what can be entered in text box: https://stackoverflow.com/questions/469357/html-text-input-allow-only-numeric-input
+    
     // 2. Attract more attention to the newName shown
     // 3. Some more paragraphs below form about name, day of week of birth, etc
     // 4. Allow calendar picker to populate input
@@ -74,4 +74,45 @@ function displayNewName(newName) {
     // 6. Edit menu to be more relevant.
 }
 
+// 1. Limit what can be entered textbox to the given inputFilter function: https://stackoverflow.com/questions/469357/html-text-input-allow-only-numeric-input
+function setInputFilter(textbox, inputFilter, errMsg) {
+    [ "input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop", "focusout" ].forEach(function(event) {
+      textbox.addEventListener(event, function(e) {
+        if (inputFilter(this.value)) {
+          // Accepted value.
+          if ([ "keydown", "mousedown", "focusout" ].indexOf(e.type) >= 0){
+            this.classList.remove("input-error");
+            this.setCustomValidity("");
+          }
+  
+          this.oldValue = this.value;
+          this.oldSelectionStart = this.selectionStart;
+          this.oldSelectionEnd = this.selectionEnd;
+        }
+        else if (this.hasOwnProperty("oldValue")) {
+          // Rejected value: restore the previous one.
+          this.classList.add("input-error");
+          this.setCustomValidity(errMsg);
+          this.reportValidity();
+          this.value = this.oldValue;
+          this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+        }
+        else {
+          // Rejected value: nothing to restore.
+          this.value = "";
+        }
+      });
+    });
+  }
 
+  setInputFilter(document.getElementById("dd"), function(value) {
+  return /^\d*$/.test(value) && (value === "" || parseInt(value) <= 31); // Allow digits only, and up to 31 max
+}, "Only 1 - 31 is allowed");
+
+setInputFilter(document.getElementById("mm"), function(value) {
+    return /^\d*$/.test(value) && (value === "" || parseInt(value) <= 12); // Allow digits only, and up to 12 max
+  }, "Only 1 - 12 is allowed");
+
+  setInputFilter(document.getElementById("yyyy"), function(value) {
+    return /^\d*$/.test(value); // Allow digits only
+  }, "Only digits are allowed");
